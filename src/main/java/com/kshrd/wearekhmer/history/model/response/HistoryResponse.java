4f85fdd8c9aa86fd84e3d539f2566849fc86daaf
Me Thyvidijame <jameleo7777@gmail.com>
2023-05-21 01:37:09 +0700

    fix History endpoint

--- src/main/java/com/kshrd/wearekhmer/history/model/response/HistoryResponse.java
+++ src/main/java/com/kshrd/wearekhmer/history/model/response/HistoryResponse.java
@@ -1,6 +1,7 @@
 package com.kshrd.wearekhmer.history.model.response;
 
 import com.kshrd.wearekhmer.article.model.entity.Article;
+import com.kshrd.wearekhmer.article.response.ArticleResponse;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,8 +18,7 @@ public class HistoryResponse {
 
     private String historyId;
     private String userId;
-
-    private Article article;
+    private ArticleResponse article;
 
     private Timestamp createdAt;
 }
