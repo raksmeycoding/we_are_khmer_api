1309d556543ff32619bf05319cb786872e935780
raksmey koung <raksmeykoung@gmail.com>
2023-07-08 00:32:08 +0700

    fix api swagger server

--- src/main/java/com/kshrd/wearekhmer/config/SecurityConfig.java
+++ src/main/java/com/kshrd/wearekhmer/config/SecurityConfig.java
@@ -2,9 +2,13 @@ package com.kshrd.wearekhmer.config;
 
 
 import com.kshrd.wearekhmer.utils.WeAreKhmerCurrentUser;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -22,6 +26,11 @@ public class SecurityConfig {
         return new WeAreKhmerCurrentUser();
     }
 
-
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setUrl("https://api.domrra.site");
+        return new OpenAPI().servers(List.of(server));
+    }
 
 }
